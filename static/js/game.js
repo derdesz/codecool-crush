a0af9ade31d167acdbf1e562c2840e91740027a6
Takács Veronika <roni9764@gmail.com>
2020-05-11 20:42:10 +0200

    Create basic board grid 1.0

--- static/js/game.js
+++ static/js/game.js
@@ -1,11 +1,20 @@
+let board = document.querySelector(".board");
+let width = 8;
+
+
+
 initGame();
 
 function initGame() {
-
+    createBoard();
     // Your game can start here, but define separate functions, don't write everything in here :)
-
 }
 
 function createBoard() {
-
+    for (let i=0; i < width*width; i++) {
+        cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.setAttribute('id', `c${i}`);
+        board.appendChild(cell);
+    }
 }
