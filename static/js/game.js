879ea4e6180fd511da02fad949e71857ad41331b
derdesz <dora.mailbox@gmail.com>
2020-05-12 13:05:17 +0200

    add drag and drop functions

--- static/js/game.js
+++ static/js/game.js
@@ -4,17 +4,18 @@ let width = 8;
 //    "url('/static/images/Gabor.png')", "url('/static/images/Laci.png')", "url('/static/images/Reka.png')" ]
 
 let basicCandies = ['Laci', 'Gabor', 'Agi', 'Bence', 'Reka', 'Adam'];
-
+let cells = []
 initGame();
 
 
 function initGame() {
     createBoard();
+    addDraggingFunctionForCells();
     // Your game can start here, but define separate functions, don't write everything in here :)
 }
 
 function createBoard() {
-    for (let i=0; i < width*width; i++) {
+    for (let i = 0; i < width * width; i++) {
         cell = document.createElement('div');
         cell.className = 'cell';
         // cell.setAttribute('id', `c${i}`);
@@ -28,7 +29,7 @@ function createBoard() {
         let candyType = basicCandies[randomNumber];
         cell.classList.add(candyType);
         board.appendChild(cell);
-
+        cells.push(cell)
     }
 }
 
@@ -37,3 +38,45 @@ function createRandomCandy() {
     return randomNumber;
 }
 
+function dragStart(event) {
+    console.log('dragStart');
+}
+
+function dragEnd(event) {
+    console.log('dragEnd');
+}
+
+function dragEnter(event) {
+    event.preventDefault();
+    console.log('dragEnter');
+}
+
+function dragOver(event) {
+    event.preventDefault();
+    console.log('dragOver');
+}
+
+function dragLeave(event) {
+    event.preventDefault();
+    console.log('dragLeave');
+}
+
+function dragDrop(event) {
+    event.preventDefault();
+    console.log('dragDrop');
+}
+
+function addDraggingFunctionForCells() {
+    for (let cell of cells) {
+        cell.addEventListener('dragstart', dragStart);
+        cell.addEventListener('dragend', dragEnd);
+        cell.addEventListener('dragenter', dragEnter);
+        cell.addEventListener('dragover', dragOver);
+        cell.addEventListener('dragleave', dragLeave);
+        cell.addEventListener('drop', dragDrop);
+    }
+}
+
+function movingCandies() {
+
+}
